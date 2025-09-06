328a93ad8c1c43827a853af20ea967f099560860
yvn-1 <oyvindbkaasa@gmail.com>
2025-09-06 22:44:46 +0200

    Corrected syntax

--- Skole/FYS4150/problem2.cpp
+++ Skole/FYS4150/problem2.cpp
@@ -1,27 +1,31 @@
 #include <fstream>
 #include <vector>
 
-vector<float> xvector;
-vector<float> uxvector;
+int main() {
+std::vector<float> xvector;
+std::vector<float> uxvector;
 
-double x = 0;
+double x = 0.0;
 do {
 	xvector.push_back(x);
 	x = x + 0.1
 } while {x<10};
 
-for (x: xvector) {
+for (auto x: xvector) {
 	uxvector.push_back(x);
 }
 
 std::ofstream outputFile("problem2.txt");
 
 if (outputFile.is_open()) {
-	if (size_of(xvector) == size_of(uxvector)) {
-		for (i=0; i<size_of(xvector);i++) {
+	if (xvector.size() == uxvector.size()) {
+		for (int i=0; i<xvector.size();i++) {
 			outputFile << xvector[i] + "," + uxvector << std::endl;
 		}
 	};
 };
 
 outputFile.close();
+
+return 0;
+}
