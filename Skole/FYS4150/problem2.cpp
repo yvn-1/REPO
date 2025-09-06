47cc703846ee0b5a6ddc8d85e037c30acf68c7ff
yvn-1 <oyvindbkaasa@gmail.com>
2025-09-06 22:49:09 +0200

    C

--- Skole/FYS4150/problem2.cpp
+++ Skole/FYS4150/problem2.cpp
@@ -9,18 +9,18 @@ double x = 0.0;
 do {
 	xvector.push_back(x);
 	x = x + 0.1;
-} while {x<10};
+} while (x<10);
 
-for (auto x: xvector) {
-	uxvector.push_back(x);
+for (auto e: xvector) {
+	uxvector.push_back(e);
 }
 
 std::ofstream outputFile("problem2.txt");
 
 if (outputFile.is_open()) {
 	if (xvector.size() == uxvector.size()) {
-		for (int i=0; i<xvector.size();i++) {
-			outputFile << xvector[i] + "," + uxvector << std::endl;
+		for (size_t i=0; i<xvector.size();i++) {
+			outputFile << xvector[i] << "," << uxvector << std::endl;
 		}
 	};
 };
