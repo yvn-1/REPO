6a129721d513cc8c584f9f5797492ff7f8ecdcc6
yvn-1 <oyvindbkaasa@gmail.com>
2025-09-06 23:11:20 +0200

    .

--- Skole/FYS4150/Project1/problem2.cpp
+++ Skole/FYS4150/Project1/problem2.cpp
@@ -1,18 +1,20 @@
 #include <fstream>
 #include <vector>
+#include <cmath>
 
 int main() {
 std::vector<float> xvector;
 std::vector<float> uxvector;
 
-double x = 0.0;
+double x = 0.00001;
 do {
 	xvector.push_back(x);
 	x = x + 0.1;
 } while (x<10);
 
 for (auto e: xvector) {
-	uxvector.push_back(e);
+	float val = 1.0-(1.0-std::exp(-10.0))*e-std::exp(-10.0*e);
+	uxvector.push_back(val);
 }
 
 std::ofstream outputFile("problem2.txt");
