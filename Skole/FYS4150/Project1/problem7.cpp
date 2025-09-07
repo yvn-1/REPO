e38e5db3d244e71935db94ee5a00be9a2c6ca30e
yvn-1 <oyvindbkaasa@gmail.com>
2025-09-08 01:12:21 +0200

    Update

--- Skole/FYS4150/Project1/problem7.cpp
+++ Skole/FYS4150/Project1/problem7.cpp
@@ -2,30 +2,29 @@
 #include <cmath>
 #include <vector>
 #include <iomanip>
+
+float f(float x) {return std::expf(-10*x);}
+
 int main() {
-float a = -1.0f;
-float c = -1.0f;
-int N = 100;
-int n = N-1;
-std::vector<float> b(n, 2.0f);
+	float a = -1.0f;
+	float c = -1.0f;
+	int N = 100;
+	int n = N-1;
+	std::vector<float> b(n, 2.0f);
 
-float h = 1.0f/N;
+	float h = 1.0f/N;
 
-vector<float> x(n);
-for (int i = 0; i<n;i++) {
-	x[i] = i*h;
-};
+	std::vector<float> x(n);
+	for (int i = 0; i<n;i++) {
+		x[i] = (i+1)*h;
+	};
 
-float f(float x) {
-	return std::expf(-10*x)
-};
+	std::vector<float> u;
 
-std::vector<float> u;
+	for (auto e: x) {
+		u.push_back(h*h*f(e));
+	};
 
-for (auto e: x) {
-	u.push_back(f(e));
-};
-
-return 0;
+	return 0;
 }
 
