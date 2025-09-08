46e53527d977973f433845a94750d5ab8dea4489
yvn-1 <oyvindbkaasa@gmail.com>
2025-09-08 02:05:33 +0200

    Update

--- Skole/FYS4150/Project1/problem7.py
+++ Skole/FYS4150/Project1/problem7.py
@@ -7,8 +7,8 @@ v = []
 
 for line in file:
     vals = line.split(",")
-    x.append(vals[0])
-    v.append(vals[1])
+    x.append(float(vals[0]))
+    v.append(float(vals[1]))
 
 plt.plot(x,v)
 plt.savefig("problem7a.pdf")
