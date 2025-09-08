01c65afa59e0602dfaa22d2c9ea39f071d2fbafd
yvn-1 <oyvindbkaasa@gmail.com>
2025-09-08 20:19:14 +0200

    a

--- Skole/FYS4150/Project1/problem7b.py
+++ Skole/FYS4150/Project1/problem7b.py
@@ -15,6 +15,15 @@ sizes = [10,100,1000,10000,100000]
 for s in sizes:
     xv = handle_file(s)
     plt.plot(xv[0],xv[1],label=f"N={s}")
+fil = open("problem2.txt")
+
+x =[]
+v = []
+for l in fil:
+    vals = l.split(",")
+    x.append(float(vals[0]))
+    v.append(float(vals[1]))
+plt.plot(x,v,label="Analytical")
 
 plt.xlabel('x')
 plt.ylabel('v_i')
