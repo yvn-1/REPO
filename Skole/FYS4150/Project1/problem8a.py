5cf134a5c007d818823ab2499b124d4c05066182
yvn-1 <oyvindbkaasa@gmail.com>
2025-09-08 22:45:35 +0200

    a

--- Skole/FYS4150/Project1/problem8a.py
+++ Skole/FYS4150/Project1/problem8a.py
@@ -15,7 +15,7 @@ def read_and_analyze(filename):
         u.append(1-(1-np.exp(-10))*e-np.exp(-10*e))
     err = []
     for i in range(1,len(x)):
-        err.append(np.log10(abs(u[i]-v[i])))
+        err.append(np.maximum(np.log10(abs(u[i]-v[i])),1e-300))
     plt.plot(x[1:],err,label=f"Error when N={len(x)}")
 
 read_and_analyze("problem710.txt")
