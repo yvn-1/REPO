976b225177e8db19ab728d7ac5014e8a6e4b8f68
yvn-1 <oyvindbkaasa@gmail.com>
2025-09-08 19:53:42 +0200

    a

---
+++ Skole/FYS4150/Project1/problem7b.cpp
@@ -0,0 +1,55 @@
+#include <fstream>
+#include <cmath>
+#include <vector>
+#include <iomanip>
+#include <string>
+
+float f(float x) {return std::expf(-10*x);};
+
+void solve(int N) {
+	float a = -1.0f;
+	float c = -1.0f;
+	int n = N-1;
+	std::vector<float> b(n, 2.0f);
+	std::vector<float> v(n);
+
+	float h = 1.0f/N;
+	
+	std::vector<float> x(n);
+	x[0] = 0;
+	for (int i = 1; i<n;i++) {
+		x[i] = i*h;
+	};
+
+	std::vector<float> u(n);
+
+	for (int i = 0; i < n; i++) {
+		u[i] = 100.0*h*h*f(x[i]);
+	};
+
+	for (int i = 1; i<n; i++) {
+		float m = a/b[i-1];
+		b[i] = b[i] - m*c;
+		u[i] = u[i] - m*u[i-1];
+	};
+	v[0] = 0;
+	v[n-1] = u[n-1] / b[n-1];
+	for (int i = n-2; i > 0; i--) {
+		v[i] = (u[i]-c*v[i+1])/b[i];
+	};
+	std::string filename = "problem7"+std::to_string(N)+".txt";
+	std::ofstream file(filename);
+	for (int i = 0; i < n; i++) {
+		file << x[i] << "," << v[i] <<std::endl;
+	};
+	file.close();
+};
+
+int main() {
+	solve(10);
+	solve(100);
+	solve(1000);
+	solve(10000);
+	solve(100000);
+}
+
