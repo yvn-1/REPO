e6be50ff083ae35f4d1aa855195530e0a5d72738
yvn-1 <oyvindbkaasa@gmail.com>
2025-09-08 22:27:34 +0200

    a

---
+++ Skole/FYS4150/Project1/problem8a.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def read_and_analyze(filename):
+    fil = open(filename)
+    x = []
+    v = []
+    for l in fil:
+        vals = l.split(",")
+        x.append(float(vals[0]))
+        v.append(float(vals[1]))
+    u = []
+    for e in x:
+        u.append(1-(1-np.exp(-10))*e-np.exp(-10*e))
+    err = []
+    for i in range(len(x)):
+        err.append(np.log10(abs(u[i]-v[i])))
+    plt.plot(x,err,label=f"Error when N={len(x)}")
+
+read_and_analyze("problem710.txt")
+read_and_analyze("problem7100.txt")
+read_and_analyze("problem71000.txt")
+read_and_analyze("problem710000.txt")
+read_and_analyze("problem7100000.txt")
+
+plt.grid(True)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.savefig("problem8a.pdf")
+
+
