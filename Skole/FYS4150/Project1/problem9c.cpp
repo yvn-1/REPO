85c1a97892644a4891de991f183966e8991c9c44
yvn-1 <oyvindbkaasa@gmail.com>
2025-09-10 20:25:56 +0200

    a

---
+++ Skole/FYS4150/Project1/problem9c.cpp
@@ -0,0 +1,64 @@
+#include <fstream>
+#include <cmath>
+#include <vector>
+#include <iomanip>
+#include <string>
+
+long double f(long double x) {return 100.0L * std::expl(-10L*x);};
+
+void solve(int N) {
+	int n = N-1;
+	std::vector<long double> b(n, 2.0f);
+	std::vector<long double> v(n);
+
+	long double h = 1.0f/N;
+	
+	std::vector<long double> x(n);
+	x[0] = 0;
+	for (int i = 0; i<n;i++) {
+		x[i] = (i+1)*h;
+	};
+
+	std::vector<long double> u(n);
+
+	for (int i = 0; i < n; i++) {
+		u[i] = h*h*f(x[i]);
+	};
+	u[0]=0;
+	for (int i = 1; i<n; i++) {
+		long double m = -1/b[i-1];
+		b[i] = b[i] + m;
+		u[i] = u[i] - m*u[i-1];
+	};
+	v[n-1] = u[n-1] / b[n-1];
+	for (int i = n-2; i >= 0; i--) {
+		v[i] = (u[i]+v[i+1])/b[i];
+	};
+
+	std::string filename = "problem9c"+std::to_string(N)+".txt";
+	std::ofstream file(filename);
+	file << std::scientific << std::setprecision(32);
+	if (size(error)<5000){
+		for (int i = 0; i < n; i++) {
+			file << x[i] << "," << v[i] <<std::endl;
+		};
+	}
+	else {
+		int step = std::floor(size(v)/5000);
+		for (int j = 0; j<n; j = j + step) {
+			file << x[j] << "," << v[j] << std::endl;
+		};
+	};
+	file.close();
+};
+
+int main() {
+	solve(10);
+	solve(100);
+	solve(1000);
+	solve(10000);
+	solve(100000);
+	solve(1000000);
+	solve(10000000);
+}
+
