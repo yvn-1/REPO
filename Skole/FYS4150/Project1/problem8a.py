d268591f19b20c0d4c8593b0844880e00b7e414f
yvn-1 <oyvindbkaasa@gmail.com>
2025-09-10 19:06:31 +0200

    a

--- Skole/FYS4150/Project1/problem8a.py
+++ Skole/FYS4150/Project1/problem8a.py
@@ -12,12 +12,12 @@ def read_and_analyze(filename,N):
         error.append(float(vals[1]))
     plt.plot(x,error,label=f"Error when N={N}")
 
-#read_and_analyze("problem710.txt",10)
-#read_and_analyze("problem7100.txt",100)
-#read_and_analyze("problem71000.txt",1000)
-#read_and_analyze("problem710000.txt",10000)
-#read_and_analyze("problem7100000.txt",100000)
-#read_and_analyze("problem71000000.txt",1000000)
+read_and_analyze("problem710.txt",10)
+read_and_analyze("problem7100.txt",100)
+read_and_analyze("problem71000.txt",1000)
+read_and_analyze("problem710000.txt",10000)
+read_and_analyze("problem7100000.txt",100000)
+read_and_analyze("problem71000000.txt",1000000)
 read_and_analyze("problem710000000.txt",10000000)
 
 plt.grid(True)
