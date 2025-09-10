bffa11908a043c6c654e8baebbe8cd7fe8a58646
yvn-1 <oyvindbkaasa@gmail.com>
2025-09-10 18:58:36 +0200

    a

--- Skole/FYS4150/Project1/problem8a.py
+++ Skole/FYS4150/Project1/problem8a.py
@@ -5,19 +5,12 @@ import numpy as np
 def read_and_analyze(filename,N):
     fil = open(filename)
     x = []
-    v = []
+    error = []
     for l in fil:
         vals = l.split(",")
         x.append(float(vals[0]))
-        v.append(float(vals[1]))
-    u = []
-    for e in x:
-        u.append(1-(1-np.exp(-10))*e-np.exp(-10*e))
-    err = []
-    for i in range(1,len(x)):
-        diff = abs(u[i]-v[i])
-        err.append(np.log10(diff))
-    plt.plot(x[1:],err,label=f"Error when N={N}")
+        error.append(float(vals[1]))
+    plt.plot(x,error,label=f"Error when N={N}")
 
 read_and_analyze("problem710.txt",10)
 read_and_analyze("problem7100.txt",100)
--- Skole/FYS4150/Project1/problem8b.cpp
+++ Skole/FYS4150/Project1/problem8b.cpp
@@ -36,16 +36,7 @@ void solve(int N) {
 	for (int i = n-2; i >= 0; i--) {
 		v[i] = (u[i]-c*v[i+1])/b[i];
 	};
-	std::vector<long double> exact;
-	for (auto e: x) {
-		double long val = 1-(1-std::expl(-10.0L))*e-std::expl(-10*e);
-		exact.push_back(val);
-	};
-	std::vector<double long> error;
-	for (int i =0; i< size(exact);i++) {
-		double long err = std::log10l(std::abs(exact[i]-v[i]));
-		error.push_back(err);
-	};
+
 	
 
 	std::string filename = "problem7"+std::to_string(N)+".txt";
@@ -53,7 +44,7 @@ void solve(int N) {
 	file << std::scientific << std::setprecision(20);
 	if (size(v)<5000){
 		for (int i = 0; i < n; i++) {
-			file << x[i] << "," << error[i] <<std::endl;
+			file << x[i] << "," << v[i] <<std::endl;
 		};
 	}
 	else {
